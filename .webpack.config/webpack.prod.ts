72a172bf77db3d94d8db727102af064a31eb94f1
Raiondesu <a.raiondesu@gmail.com>
2018-09-20 22:11:17 +0300

    Contenthash only in production

--- .webpack.config/webpack.prod.ts
+++ .webpack.config/webpack.prod.ts
@@ -4,6 +4,10 @@ import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import base from './webpack.base';
 
 export default merge(base('production'), {
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+
   performance: {
     hints: 'warning'
   },
