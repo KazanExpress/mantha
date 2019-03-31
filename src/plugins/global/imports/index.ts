ffdac1ec1d53e77a6a686cc1aad235abeff14f95
Karen <dev.kaskar2008@gmail.com>
2019-03-31 21:44:52 +0300

    Remove [plugins]: unnecessary plugins
    * advancedInheritance
    * global/imports/generateChunks
    * global/imports/importFactory
    * global/imports/use
    * global/imports/index

--- src/plugins/global/imports/index.ts
+++
@@ -1,7 +0,0 @@
-import { importFactory } from './importFactory';
-
-export const importComponent = importFactory('components', 'color: blue');
-export const importPage = importFactory('pages', 'color: #e53935');
-
-export { importFactory };
-export * from './use';
